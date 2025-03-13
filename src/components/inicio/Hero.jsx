a29968a104d5c7801c18889fdbc48b06adc94e7d
Alejandro <alejandrosantandertoto@gmail.com>
2025-03-13 17:54:16 -0500

    NEW: modificaciones algunas cosas

--- src/components/inicio/Hero.jsx
+++ src/components/inicio/Hero.jsx
@@ -1,11 +1,11 @@
-import tradeLibro from '../../assets/images/tradeLibro.png';
+import tradeLibro from '../../assets/images/imageLibraryRef.jpg';
 import Poligonos from './Poligonos';
 import '../../styles/hero.css';
 const Hero = () => {
     return (
         <div>
             <header className='hero'>
-                <h1>¡Bienvenido a nuestra página!</h1>
+                <h1 className='text-hero'>¡Bienvenido a nuestra página!</h1>
                 <p>Descubre todos los servicios que podemos ofrecerte</p>
                 <Poligonos />
                 <img className='img-hero' src= {tradeLibro} alt="" />
