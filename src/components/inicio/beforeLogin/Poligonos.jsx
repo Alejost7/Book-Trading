4c930840968249e1ccad85e557f47be4c4cbc7d4
Alejandro <alejandrosantandertoto@gmail.com>
2025-03-14 13:38:47 -0500

    New: más icons

--- src/components/inicio/beforeLogin/Poligonos.jsx
+++ src/components/inicio/beforeLogin/Poligonos.jsx
@@ -1,6 +1,8 @@
 import '../../../styles/beforeLogin/poligonos.css';
 import perfilIcon from '../../../assets/icons/avatar-de-hombre.png';
 import ayuda from '../../../assets/icons/ayuda.png';
+import sobreNosotros from '../../../assets/icons/sobre-nosotros.png';
+import libroIcon from '../../../assets/icons/lectura.png';
 const Poligonos = () => {
     return(
         <div className="poligonos">
@@ -10,8 +12,12 @@ const Poligonos = () => {
             <div className="poligono poligono2">
                 <img src={ayuda}></img>
             </div>
-            <div className="poligono poligono3"></div>
-            <div className="poligono poligono4"></div>
+            <div className="poligono poligono3">
+                <img src={sobreNosotros}></img>
+            </div>
+            <div className="poligono poligono4">
+                <img src={libroIcon}></img>
+            </div>
         </div>
     );
 };
