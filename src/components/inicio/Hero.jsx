3d512c0259b5520c74c34a181aee42a4d3c4be98
Alejandro <alejandrosantandertoto@gmail.com>
2025-03-13 19:42:20 -0500

    NEW: se agregan cosas a la página de inicio

--- src/components/inicio/Hero.jsx
+++ src/components/inicio/Hero.jsx
@@ -1,4 +1,3 @@
-import tradeLibro from '../../assets/images/imageLibraryRef.jpg';
 import Poligonos from './Poligonos';
 import '../../styles/hero.css';
 import servicios from '../../assets/videos/nuestrosServicios.mp4';
@@ -6,8 +5,6 @@ const Hero = () => {
     return (
         <div>
             <header className='hero'>
-                <h1 className='text-hero'>¡Bienvenido a nuestra página!</h1>
-                <p>Descubre todos los servicios que podemos ofrecerte</p>
                 <Poligonos />
                 <video className="video-hero" autoPlay loop muted>
                     <source src={servicios} type="video/mp4" />
