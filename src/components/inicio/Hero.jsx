ec8601dcaf93f830b15236f4615301ff1ac634e0
Alejandro <alejandrosantandertoto@gmail.com>
2025-03-13 19:07:25 -0500

    NEW: modificaciones al componente hero

--- src/components/inicio/Hero.jsx
+++ src/components/inicio/Hero.jsx
@@ -1,6 +1,7 @@
 import tradeLibro from '../../assets/images/imageLibraryRef.jpg';
 import Poligonos from './Poligonos';
 import '../../styles/hero.css';
+import servicios from '../../assets/videos/nuestrosServicios.mp4';
 const Hero = () => {
     return (
         <div>
@@ -8,7 +9,10 @@ const Hero = () => {
                 <h1 className='text-hero'>¡Bienvenido a nuestra página!</h1>
                 <p>Descubre todos los servicios que podemos ofrecerte</p>
                 <Poligonos />
-                <img className='img-hero' src= {tradeLibro} alt="" />
+                <video className="video-hero" autoPlay loop muted>
+                    <source src={servicios} type="video/mp4" />
+                    Tu navegador no soporta la reproducción de videos.
+                </video>
             </header>
         </div>
     );
