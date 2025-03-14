fb3486ecaa558b673c0661b3e28085011a3aeaba
Alejandro <alejandrosantandertoto@gmail.com>
2025-03-13 20:03:18 -0500

    NEW: cosas

--- src/components/inicio/Poligonos.jsx
+++ src/components/inicio/Poligonos.jsx
@@ -2,8 +2,8 @@ import '../../styles/poligonos.css';
 const Poligonos = () => {
     return(
         <div className="poligonos">
-            <div className="poligono poligono1">Polígono 1</div>
-            <div className="poligono poligono2">Polígono 2</div>
+            <div className="poligono poligono1"></div>
+            <div className="poligono poligono2"></div>
             <div className="poligono poligono3">Polígono 3</div>
             <div className="poligono poligono4">Polígono 4</div>
         </div>
