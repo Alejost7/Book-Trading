3f915b21455cbe9a2674fa83b7740bdf74d0a4b0
Samuel Alberto Mateo Bonilla Franco <samuelbonillaf@javerianacali.edu.co>
2025-03-15 16:42:34 -0500

    NEW:Donaciones

---
+++ src/components/donaciones/donaciones.jsx
@@ -0,0 +1,11 @@
+const donaciones = () => {
+    return(
+        <div>
+            <h1>
+                Hello World donaciones here!
+            </h1>
+        </div>
+    )
+}
+
+export default donaciones
