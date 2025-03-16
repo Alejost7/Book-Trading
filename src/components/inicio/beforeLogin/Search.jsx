980fe880e08501561be0df652f9aebaf8b293f9a
Alejandro <alejandrosantandertoto@gmail.com>
2025-03-16 18:52:19 -0500

    MOD: solucionado el error de que a veces no inicia

--- src/components/inicio/beforeLogin/Search.jsx
+++ src/components/inicio/beforeLogin/Search.jsx
@@ -1,7 +1,7 @@
 import searchIcon from '../../../assets/icons/searchIcon.png';
 const Search = () => {
     return (
-        <div class="search">
+        <div className="search">
             <input type="text" placeholder="Busca lo mejor para ti..." class="searchInput"/>
             <img src={searchIcon} class="searchIcon" />
         </div>
