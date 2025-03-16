0a1ae10c3c8d4825f2e80741ff32f7b10b5780fd
Alejandro <alejandrosantandertoto@gmail.com>
2025-03-16 18:56:18 -0500

    MOD: solucion error chiquito

--- src/components/inicio/beforeLogin/Search.jsx
+++ src/components/inicio/beforeLogin/Search.jsx
@@ -3,7 +3,7 @@ const Search = () => {
     return (
         <div className="search">
             <input type="text" placeholder="Busca lo mejor para ti..." class="searchInput"/>
-            <img src={searchIcon} class="searchIcon" />
+            <img src={searchIcon} class="searchIcon" alt="Search Icon" />
         </div>
     )
 }
