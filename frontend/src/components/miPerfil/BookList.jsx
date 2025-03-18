1d6a480ed3072b4b0ea56a86aea3cc7dd4314c31
Jorge Osorio <jorluos@hotmail.com>
2025-03-18 08:12:13 -0500

    mods

--- frontend/src/components/miPerfil/BookList.jsx
+++ frontend/src/components/miPerfil/BookList.jsx
@@ -2,13 +2,16 @@ import "../../styles/miPerfil/BookList.css";
 
 const BookList = ({ books }) => {
     return (
-        <div className="BookList">
-            {books.map((book, index) => (
-                <div key={index} className="BookCard">
-                    <img src={book.image} alt={book.name} className="BookImage" />
-                    <h3 className="BookTitle">{book.name}</h3>
-                </div>
-            ))}
+        <div className="BookListContainer">
+            <h2 className="BookListTitle">Mis Libros</h2>
+            <div className="BookList">
+                {books.map((book, index) => (
+                    <div key={index} className="BookCard">
+                        <img src={book.image} alt={book.name} className="BookImage" />
+                        <h3 className="BookTitle">{book.name}</h3>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
--- frontend/src/components/miPerfil/MiPerfil.jsx
+++ frontend/src/components/miPerfil/MiPerfil.jsx
@@ -9,7 +9,12 @@ import BookList from "./BookList"
 const misLibros = [
     {image: "https://m.media-amazon.com/images/I/81t2CVWEsUL.jpg", name: "Harry Potter"},
     {image: "https://m.media-amazon.com/images/I/71kxa1-0mfL.jpg", name: "1984"},
-    {image: "https://www.polifemo.com/static/img/portadas/_visd_0000JPG030DW.jpg", name: "Cien anhos de soledad"}
+    {image: "https://www.polifemo.com/static/img/portadas/_visd_0000JPG030DW.jpg", name: "Cien anhos de soledad"},
+    {image: "https://www.laguiadelvaron.com/wp-content/uploads/2020/11/it-1-1127x2048.jpg", name: "IT"},
+    {image: "https://www.radioacktiva.com/wp-content/uploads/2020/05/El-Principito.-640x983.jpg", name: "El Principito"},
+    {image: "https://www.okchicas.com/wp-content/uploads/2018/04/libros-que-nos-hicieron-lectoras-1.jpg", name: "Diario de Ana Frank"},
+    
+
 ]
 
 const MiPerfil = () => {
@@ -27,6 +32,7 @@ Soy estudiante de **Ingeniería de Sistemas y Computación** en la *Universidad 
 *"El conocimiento no tiene límites, solo los que tú le pongas."*
                                     `}/>
                 </div>
+                <h1></h1>
                 <div className="container-books">
                     <BookList books={misLibros}/>
                 </div>
