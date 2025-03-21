3651fe4dadfdb8d01c20f640183fe5f3916461ed
Jorge Luis Osorio Quiroga <148157250+jorluos1@users.noreply.github.com>
2025-03-21 12:14:57 -0500

    MODS MI PERFIL

--- frontend/src/components/miPerfil/MiPerfil.jsx
+++ frontend/src/components/miPerfil/MiPerfil.jsx
@@ -12,8 +12,7 @@ const misLibros = [
     {image: "https://m.media-amazon.com/images/I/71kxa1-0mfL.jpg", name: "1984"},
     {image: "https://www.polifemo.com/static/img/portadas/_visd_0000JPG030DW.jpg", name: "Cien anhos de soledad"},
     {image: "https://www.laguiadelvaron.com/wp-content/uploads/2020/11/it-1-1127x2048.jpg", name: "IT"},
-    {image: "https://www.radioacktiva.com/wp-content/uploads/2020/05/El-Principito.-640x983.jpg", name: "El Principito"},
-    {image: "https://www.okchicas.com/wp-content/uploads/2018/04/libros-que-nos-hicieron-lectoras-1.jpg", name: "Diario de Ana Frank"},
+    {image: "https://www.radioacktiva.com/wp-content/uploads/2020/05/El-Principito.-640x983.jpg", name: "El Principito"}
     
 
 ]
